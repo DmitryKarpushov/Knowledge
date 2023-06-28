a59f4086b493f06135e40a9e5e5ee115617859d5
Dmitry <dmitr.karpushoff2016@yandex>
2023-06-28 22:21:18 +0300

    Add project

---
+++ src/test/java/ru/kb/Knowledge/KnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kb.Knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
